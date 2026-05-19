fcfd7a58a962df6627f2c3cd6be23640326eaa82
gpalsingh <12171804+gpalsingh@users.noreply.github.com>
2026-05-19 15:30:32 +0530

    fix: Linux release

--- Formula/cm-tools.rb
+++ Formula/cm-tools.rb
@@ -9,6 +9,12 @@ class CmTools < Formula
   elsif OS.mac? && Hardware::CPU.intel?
     url "https://github.com/commutatus/homebrew-tools/releases/download/cm-tools-v0.1.1/cm-tools_darwin_amd64.tar.gz"
     sha256 "c184464cc8da7cff5a9ee32974433aaa78fbf4e4e9c17826293814be7b0112c4"
+  elsif OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+    url "https://github.com/commutatus/homebrew-tools/releases/download/cm-tools-v0.1.1/cm-tools_linux_arm64.tar.gz"
+    sha256 "placeholder_linux_arm64_sha256"
+  elsif OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/commutatus/homebrew-tools/releases/download/cm-tools-v0.1.1/cm-tools_linux_amd64.tar.gz"
+    sha256 "placeholder_linux_amd64_sha256"
   end
 
   def install
